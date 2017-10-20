0955f79e5f952551b4085f89288a23a6fb8b4567
Roger Tónlist <roger.tonlist@gmail.com>
2017-10-20 12:55:03 +0200

    Mer spelare

--- src/pages/players/players.ts
+++ src/pages/players/players.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import PouchDB from 'pouchdb';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import{ Player } from '../tournament-detail/player';
 
@@ -15,14 +18,78 @@ import{ Player } from '../tournament-detail/player';
   templateUrl: 'players.html'
 })
 export class PlayersPage {
-
-  tournamentPlayers: any;
+  private globalPlayers: any;
+  private database: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.database = new PouchDB('nr_global_players');
+    console.log(this.database);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PlayersPage');
+
+    this.allPlayers().then((data) => {
+      this.globalPlayers = data;
+      console.log(this.globalPlayers);
+    });
+  }
+  public allPlayers() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+
+      this.database.allDocs({
+        include_docs: true
+      }).then((result) => {
+        this.data = [];
+        let docs = result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+        console.log(this.data);
+        console.log("Hit men inte längre.");
+        this.database.changes({live: true, since: 'now', include_docs: true}).on('change', (change) => {
+          this.handleChange(change);
+        });
+
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
   }
 
+  handleChange(change){
+
+     let changedDoc = null;
+     let changedIndex = null;
+
+     this.data.forEach((doc, index) => {
+
+       if(doc._id === change.id){
+         changedDoc = doc;
+         changedIndex = index;
+       }
+
+     });
+
+     //A document was deleted
+     if(change.deleted){
+       this.data.splice(changedIndex, 1);
+     }
+     else {
+       //A document was updated
+       if(changedDoc){
+         this.data[changedIndex] = change.doc;
+       }
+       //A document was added
+       else {
+         this.data.push(change.doc);
+       }
+     }
+   }
 }
